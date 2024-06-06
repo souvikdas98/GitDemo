ed3d26bfd078d6bae43d08a2031e48adeab13aaf
souvikdas98 <souvik.das0505@gmail.com>
2024-06-07 01:44:42 +0530

    this is commit from Asian timezone

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -7,5 +7,17 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src\\test\\java\\cucumber", glue="rahulshettyacademy.stepDefinations",
 monochrome=true,tags= "@Regression", plugin= {"html:target\\cucumber.html"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-	
+	public void print()
+	{
+		System.out.println("postjira");
+		System.out.println("postjira2");
+		System.out.println("postjira3");
+		
+		
+		
+		
+		
+		
+		
+	}
 }
